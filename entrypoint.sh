2d2b843de233df6afa5fba22b7616e87ad6bc5b7
Amon Caldas <contato@codigocriativo.com.br>
2021-10-13 18:20:04 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,9 @@ echo "$time Run: npm install"
 npm install
 
 time=$(date)
+echo "$time Run: npm rebuild node-sass"
+npm install node-sass && npm rebuild node-sass
+
+time=$(date)
 echo "$time Run: npm run $target"
 npm run "$target"
