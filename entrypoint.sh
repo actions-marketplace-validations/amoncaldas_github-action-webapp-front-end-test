3890e56d4996ac660db414f3450e2139a8c3d85d
Amon Caldas <amoncaldas@gmail.com>
2021-10-13 19:03:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 target=$1
 
 time=$(date)
-echo "$time Run: npm install"
-npm install
+echo "$time Run: npm ci"
+npm ci
 
 time=$(date)
 echo "$time Run: npm rebuild node-sass"
-npm install node-sass && npm rebuild node-sass
+npm rebuild node-sass
 
 time=$(date)
 echo "$time Run: npm run $target"
